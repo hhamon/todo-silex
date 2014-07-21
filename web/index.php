1daa33a1d430c5e7a5e794d37e429c3b74699cb8
Hugo Hamon <hugo.hamon@sensiolabs.com>
2014-07-21 18:19:51 +0200

    Implemented list and details actions.

--- web/index.php
+++ web/index.php
@@ -3,13 +3,14 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $app = new Application([ 'debug' => true ]);
 $app->register(new TwigServiceProvider(), [
-    'twig.path' => __DIR__.'/../views',
+    'twig.path'            => __DIR__.'/../views',
     'twig.options'         => [
         'cache'            => __DIR__.'/../cache/twig',
         'debug'            => $app['debug'],
@@ -17,25 +18,42 @@ $app->register(new TwigServiceProvider(), [
         'auto_reload'      => $app['debug'],
     ],
 ]);
+$app->register(new DoctrineServiceProvider(), [
+    'db.options' => [
+        'driver'    => 'pdo_mysql',
+        'host'      => 'localhost',
+        'dbname'    => 'training_todo',
+        'user'      => 'root',
+        'password'  => '',
+        'charset'   => 'utf8',
+    ],
+]);
 
 // Homepage
 $app
-    ->get('/', function () {
-        
+    ->get('/', function () use ($app) {
+        return $app['twig']->render('index.html.twig', [
+            'count' => $app['db']->fetchColumn('SELECT COUNT(*) FROM todo'),
+            'todos' => $app['db']->fetchAll('SELECT * FROM todo'),
+        ]);
     })
     ->bind('homepage')
 ;
 
 // Show one task
 $app
-    ->get('/todo/{id}', function ($id) {
-        
+    ->get('/todo/{id}', function ($id) use ($app) {
+        $todo = $app['db']->fetchAssoc('SELECT * FROM todo WHERE id = ?', [ $id ]);
+        if (!$todo) {
+            $app->abort(404, sprintf('Todo #%u does not exist.', $id));
+        }
+
+        return $app['twig']->render('todo.html.twig', [ 'todo' => $todo ]);
     })
     ->bind('todo')
     ->assert('id', '\d+')
     //->comment('a comment to explain the feature')
 ;
-
 
 // Create a new task
 $app
