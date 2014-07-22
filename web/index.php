07c9c479a8029178a2e19a6847405ca036f577c5
Hugo Hamon <hugo.hamon@sensiolabs.com>
2014-07-22 16:40:14 +0200

    Added YAML config for routes

--- web/index.php
+++ web/index.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Flint\Provider\TackerServiceProvider;
+use Flint\Provider\RoutingServiceProvider;
 use Todo\Provider\TodoServiceProvider;
 use Todo\Provider\TodoControllerProvider;
 use Silex\Application;
@@ -16,10 +18,19 @@ $app = new Application([ 'debug' => true ]);
 $app->register(new TodoServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new UrlGeneratorServiceProvider());
+$app->register(new TackerServiceProvider(), [
+    'root_dir' => __DIR__.'/..',
+]);
+$app->register(new RoutingServiceProvider, [
+    'routing.resource' => $app['root_dir'].'/config/routing.yml',
+    'routing.options'  => [
+        'cache_dir' => $app['root_dir'].'/cache',
+    ],
+]);
 $app->register(new TwigServiceProvider(), [
-    'twig.path'            => __DIR__.'/../views',
+    'twig.path'            => $app['root_dir'].'/views',
     'twig.options'         => [
-        'cache'            => __DIR__.'/../cache/twig',
+        'cache'            => $app['root_dir'].'/cache/twig',
         'debug'            => $app['debug'],
         'strict_variables' => $app['debug'],
         'auto_reload'      => $app['debug'],
@@ -38,12 +49,12 @@ $app->register(new DoctrineServiceProvider(), [
 
 if ($app['debug']) {
     $app->register(new WebProfilerServiceProvider(), [
-        'profiler.cache_dir' => __DIR__.'/../cache/profiler',
+        'profiler.cache_dir' => $app['root_dir'].'/cache/profiler',
     ]);
 }
 
 // Register routes
-$app->mount('/todo', new TodoControllerProvider());
+//$app->mount('/todo', new TodoControllerProvider());
 
 // Run the application
 $app->run(Request::createFromGlobals());
